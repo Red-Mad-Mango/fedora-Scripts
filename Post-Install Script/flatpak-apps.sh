7b1036f7bae3de51aa38ba6b7c4eed11684c4e26
Red-Mad-Mango <227115390+Red-Mad-Mango@users.noreply.github.com>
2026-09-07 14:55:37 +0530

    Update flatpak-apps.sh

--- Post-Install Script/flatpak-apps.sh
+++ Post-Install Script/flatpak-apps.sh
@@ -2,39 +2,39 @@
 set -euo pipefail
 
 # Add Flathub remote (system scope)
-flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
 apps=(
-  com.github.tchx84.Flatseal #flatseal      
+  com.github.tchx84.Flatseal #flatseal
   com.heroicgameslauncher.hgl #heroic game launcher
- # org.keepassxc.KeePassXC #KeepassXC
   com.github.jeromerobert.pdfarranger #pdfarranger
- # org.localsend.localsend_app
   io.gitlab.metadatacleaner.metadatacleaner #metadatacleaner
   page.codeberg.censor.Censor #censor
   org.inkscape.Inkscape #inkscape
-  # org.gimp.GIMP #gimp
   org.kde.kdenlive #kdenlive
-  # org.gnome.Music #gnome-music
+  org.gnome.Music #gnome-music
   io.gitlab.theevilskeleton.Upscaler #upscaler
   com.github.johnfactotum.Foliate #foliate
- # org.kde.ghostwriter
   no.bragefuglseth.Keypunch #keypunch
-  # io.github.kolunmi.Bazaar #bazaar
-  # be.alexandervanhee.gradia #gradia
+  be.alexandervanhee.gradia #gradia
   org.gnome.Chess #gnome-chess
   app.drey.EarTag #eartag
   io.github.shonebinu.Defuse #defuse
   io.github.nacho.mundi #mundi
   br.eng.silas.qpdftools #qpdftools
-  # org.gnome.gitlab.somas.Apostrophe #apostrophe
+  org.gnome.gitlab.somas.Apostrophe #apostrophe
   io.gitlab.adhami3310.Converter #switchroo
-  # org.gnome.World.Secrets #secrets
-  # io.github.wartybix.Constrict #constrict
-  com.github.wwmm.easyeffects #easy effect
+  io.github.wartybix.Constrict #constrict
   io.github.nokse22.asciidraw #ascii draw
-  # io.github.masakk1.press #audio compressor
+  io.github.masakk1.press #audio compressor
+  com.github.hugolabe.Wike #wikipedia
+  dev.mufeed.Wordbook #definitions
+  art.fatdawlf.Piccolo #color-picker
+  com.github.huluti.Curtail #img-optimizer
+  # org.localsend.localsend_app
+  # org.gimp.GIMP #gimp
   # moe.liam.audimorf #audio convertor
+  # com.github.wwmm.easyeffects #easy effect
 )
 
 for app in "${apps[@]}"; do
